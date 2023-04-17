45ce17f18ef51d8b682e299074ff74d1a2931987
dan-zayas <dmzayasplus@gmail.com>
2023-04-17 11:41:18 -0500

    Added serializers and views for the invoices

--- inventory_api/app_control/views.py
+++ inventory_api/app_control/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import (
-    Inventory, InventorySerializer, InventoryGroup, InventoryGroupSerializer, Client, ClientSerializer
+    Inventory, InventorySerializer, InventoryGroup, InventoryGroupSerializer, Client, ClientSerializer, Invoice, InvoiceSerializer
 )
 from rest_framework.response import Response
 from inventory_api.custom_methods import IsAuthenticatedCustom
@@ -93,6 +93,38 @@ class ClientView(ModelViewSet):
         if keyword:
             search_fields = (
                 "created_by__fullname", "created_by__email", "name"
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+        
+        return results
+
+    
+    def create(self, request, *args, **kwargs):
+        request.data.update({"created_by_id": request.user.id})
+        return super().create(request, *args, **kwargs)
+    
+
+class InvoiceView(ModelViewSet):
+    queryset = Invoice.objects.select_related(
+        "created_by", "client").prefetch_related("invoice_items")
+    serializer_class = InvoiceSerializer
+    permission_classes = (IsAuthenticatedCustom, )
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        if self.request.method.lower() != "get":
+            return self.queryset
+        
+        data = self.request.query_params.dict()
+        data.pop("page")
+        keyword = data.pop("keyword", None)
+
+        results = self.queryset(**data)
+
+        if keyword:
+            search_fields = (
+                "created_by__fullname", "created_by__email", "client__name"
             )
             query = get_query(keyword, search_fields)
             results = results.filter(query)
